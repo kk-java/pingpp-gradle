37b84595c375bb0142c26cf337762d280619686c
liucanwen <124597202@qq.com>
2016-03-27 02:04:05 +0800

    initial commit

---
+++ pingppsdk/src/androidTest/java/com/liucanwen/app/pingppsdk/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.liucanwen.app.pingppsdk;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
